95bff17536b59d20e71c9d58a39f21818b246204
mhbleakley <mhbleakley@wpi.edu>
2022-10-01 21:26:56 -0400

    changing devices

--- test_area.py
+++ test_area.py
@@ -1,13 +1,24 @@
-from player import Player
-from texas import Texas
+# from player import Player
+# from texas import Texas
 
-john = Player("John", 10)
-sam = Player("Sam", 10)
-aaron = Player("Aaron", 10)
-paul = Player("Paul", 10)
-gus = Player("Gus", 10)
+# john = Player("John", 10)
+# sam = Player("Sam", 10)
+# aaron = Player("Aaron", 10)
+# paul = Player("Paul", 10)
+# gus = Player("Gus", 10)
 
-players = [john, sam, aaron, paul, gus]
+# players = [john, sam, aaron, paul, gus]
 
-texas = Texas(players, 1)
-texas.test()
+# texas = Texas(players, 1)
+# texas.test()
+
+f = open("screen.txt", "r")
+
+lines = f.readlines()
+f.close()
+
+msg = ""
+for line in lines:
+    msg += line
+
+print(msg)
